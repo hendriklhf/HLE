350f40742e5cdd774f47e12713307ba7b346ad16
Sterbehilfe <twitch.tv/strbhlfe>
2021-06-27 18:17:33 +0200

    further improvements on PrefixNumber

--- Sterbehilfe/Numbers/PrefixNumber.cs
+++ Sterbehilfe/Numbers/PrefixNumber.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sterbehilfe.Numbers
 {
@@ -12,92 +8,101 @@ namespace Sterbehilfe.Numbers
 
         public UnitPrefix UnitPrefix { get; }
 
-        public string Unit { get; }
-
         public double Value => Number * UnitPrefix.Value;
 
-        public PrefixNumber(double number, UnitPrefix unitPrefix, string unit)
+        public PrefixNumber(double number, UnitPrefix unitPrefix)
         {
             Number = number;
             UnitPrefix = unitPrefix;
-            Unit = unit;
         }
 
-        public PrefixNumber(double number, UnitPrefix unitPrefix) : this(number, unitPrefix, "")
-        {
-        }
-
-        public PrefixNumber(double number, string unit) : this(number, UnitPrefix.Null, unit)
-        {
-        }
-
-        public PrefixNumber(double number) : this(number, UnitPrefix.Null, "")
+        public PrefixNumber(double number) : this(number, UnitPrefix.Null)
         {
         }
 
         public static bool operator >(PrefixNumber left, PrefixNumber right)
         {
-            return left.Number * left.UnitPrefix.Value > right.Number * right.UnitPrefix.Value;
+            return left.Value > right.Value;
         }
 
         public static bool operator <(PrefixNumber left, PrefixNumber right)
         {
-            return left.Number * left.UnitPrefix.Value < right.Number * right.UnitPrefix.Value;
+            return left.Value < right.Value;
         }
 
         public static bool operator >=(PrefixNumber left, PrefixNumber right)
         {
-            return left.Number * left.UnitPrefix.Value >= right.Number * right.UnitPrefix.Value;
+            return left.Value >= right.Value;
         }
 
         public static bool operator <=(PrefixNumber left, PrefixNumber right)
         {
-            return left.Number * left.UnitPrefix.Value <= right.Number * right.UnitPrefix.Value;
+            return left.Value <= right.Value;
         }
 
-        public static PrefixNumber operator +(PrefixNumber left, PrefixNumber right)
-        {
-            
-        }
+        //public static PrefixNumber operator +(PrefixNumber left, PrefixNumber right)
+        //{
 
-        public static PrefixNumber operator -(PrefixNumber left, PrefixNumber right)
-        {
+        //}
 
-        }
+        //public static PrefixNumber operator -(PrefixNumber left, PrefixNumber right)
+        //{
 
-        public static PrefixNumber operator *(PrefixNumber left, PrefixNumber right)
-        {
+        //}
 
-        }
+        //public static PrefixNumber operator *(PrefixNumber left, PrefixNumber right)
+        //{
 
-        public static PrefixNumber operator /(PrefixNumber left, PrefixNumber right)
-        {
+        //}
 
-        }
+        //public static PrefixNumber operator /(PrefixNumber left, PrefixNumber right)
+        //{
+
+        //}
 
         public static PrefixNumber operator ++(PrefixNumber prefixNumber)
         {
-
+            return new(prefixNumber.Number + 1, prefixNumber.UnitPrefix);
         }
 
         public static PrefixNumber operator --(PrefixNumber prefixNumber)
         {
-
+            return new(prefixNumber.Number - 1, prefixNumber.UnitPrefix);
         }
 
         public static PrefixNumber operator !(PrefixNumber prefixNumber)
         {
+            return new(-prefixNumber.Number, prefixNumber.UnitPrefix);
+        }
 
+        public static implicit operator PrefixNumber(long l)
+        {
+            return new(l);
+        }
+
+        public static implicit operator PrefixNumber(ValueTuple<long, UnitPrefix> vT)
+        {
+            return new(vT.Item1, vT.Item2);
+        }
+
+        public static implicit operator PrefixNumber(double d)
+        {
+            return new(d);
+        }
+
+        public static implicit operator PrefixNumber(ValueTuple<double, UnitPrefix> vT)
+        {
+            return new(vT.Item1, vT.Item2);
         }
 
         public bool Equals(PrefixNumber prefixNumber)
         {
-            return Number * UnitPrefix.Value == prefixNumber.Number * prefixNumber.UnitPrefix.Value;
+            return Value == prefixNumber.Value;
         }
 
         public override string ToString()
         {
-            return $"{Number} {UnitPrefix.Symbol}{Unit}".Trim();
+            return $"{Number} {UnitPrefix.Symbol}";
         }
     }
 }
--- Sterbehilfe/Numbers/UnitPrefix.cs
+++ Sterbehilfe/Numbers/UnitPrefix.cs
@@ -1,13 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.Collections.Specialized;
 using System.Collections.ObjectModel;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sterbehilfe.Numbers
 {
