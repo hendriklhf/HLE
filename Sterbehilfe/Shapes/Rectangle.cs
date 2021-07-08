04e6ed4b6f451047cc5dd22275c031664c40ddae
Sterbehilfe <twitch.tv/strbhlfe>
2021-07-08 17:27:33 +0200

    added shapes

---
+++ Sterbehilfe/Shapes/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sterbehilfe.Shapes
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double Diagonals => Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+
+        public override double Area => Width * Height;
+
+        public override double Circumference => 2 * (Width + Height);
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ Sterbehilfe/Shapes/Shape.cs
@@ -0,0 +1,9 @@
+﻿namespace Sterbehilfe.Shapes
+{
+    public abstract class Shape
+    {
+        public abstract double Area { get; }
+
+        public abstract double Circumference { get; }
+    }
+}
