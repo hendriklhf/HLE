4297eb8fe3a0d7a0d133670522f90a103c12a976
Sterbehilfe <twitch.tv/strbhlfe>
2021-07-16 21:57:31 +0200

    renamed class

--- HLE/Collections/Collection.cs
+++ HLE/Collections/Collection.cs
@@ -7,7 +7,7 @@ namespace HLE.Collections
     /// <summary>
     /// A class containing multiple collections of usefull elements.
     /// </summary>
-    public static class Collections
+    public static class Collection
     {
         private static readonly List<char> _alphabetLowerCase = new() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
         private static readonly List<char> _alphabetUpperCase = new() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
