36ea0c704ba49135faee16cf980e8728ab222e02
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-01 13:42:47 +0200

    began own twitch chat client

---
+++ HLE/Twitch/ChatClient/ClientOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLE.Twitch.ChatClient
+{
+    public class ClientOptions
+    {
+        public string Username { get; }
+
+        public string OAuthToken { get; }
+
+        public List<string> Channels { get; }
+
+        public ClientOptions(string username, string token, List<string> channels)
+        {
+            Username = username;
+            OAuthToken = token;
+            Channels = channels;
+        }
+
+        public ClientOptions(string username, string token, string channel) : this(username, token, new List<string>() { channel })
+        {
+        }
+    }
+}
