015fe0073bfbcf267f341b0b5e8d861ce33a02d1
Sterbehilfe <twitch.tv/strbhlfe>
2021-08-03 03:09:28 +0200

    moved twitch client to a seperate project

---
+++ HLETwitchClient/ChatClient/IrcClient.cs
@@ -0,0 +1,66 @@
+﻿using HLE.Strings;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HLETwitchClient.ChatClient
+{
+    public class IrcClient
+    {
+        public ClientWebSocket ClientWebSocket { get; private set; } = new();
+
+        public bool IsConnected => ClientWebSocket.State == WebSocketState.Open;
+
+        private readonly ClientOptions _options;
+        private const string _url = "wss://irc-ws.chat.twitch.tv:443";
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+        public IrcClient(ClientOptions options)
+        {
+            _options = options;
+        }
+
+        public void Connect()
+        {
+            OpenWebSocket();
+            ConnectToIRC();
+            StartListening();
+        }
+
+        private async Task SendAsync(string message)
+        {
+            if (IsConnected)
+            {
+                await ClientWebSocket.SendAsync(message.Encode(), WebSocketMessageType.Text, true, _cancellationTokenSource.Token);
+            }
+        }
+
+        private void OpenWebSocket()
+        {
+            ClientWebSocket.ConnectAsync(new(_url), _cancellationTokenSource.Token).Wait();
+        }
+
+        private void ConnectToIRC()
+        {
+            Task.Run(async () => await SendAsync($"PASS {_options.OAuthToken}"));
+            Task.Run(async () => await SendAsync($"NICK {_options.Username}"));
+        }
+
+        private void StartListening()
+        {
+            Task.Run(async () =>
+            {
+                while (IsConnected)
+                {
+                    byte[] buffer = new byte[1024];
+                    WebSocketReceiveResult result = await ClientWebSocket.ReceiveAsync(buffer, _cancellationTokenSource.Token);
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        Console.WriteLine(buffer.Decode());
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ HLETwitchClient/ChatClient/TwitchClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace HLETwitchClient.ChatClient
+{
+    public class TwitchClient
+    {
+        public string Username { get; }
+
+        public string OAuthToken { get; }
+
+        public ReadOnlyCollection<string> Channels { get; }
+
+
+        public IrcClient IrcClient { get; }
+
+        public bool IsConnected => IrcClient.IsConnected;
+
+        public event EventHandler OnConnected;
+        public event EventHandler OnDisconnected;
+        public event EventHandler OnReconnected;
+
+        private readonly ClientOptions _options;
+
+        public TwitchClient(ClientOptions options)
+        {
+            _options = options;
+            IrcClient = new(_options);
+            Username = _options.Username;
+            OAuthToken = _options.OAuthToken;
+            Channels = new(_options.Channels);
+        }
+
+        public void Connect()
+        {
+            if (!IsConnected)
+            {
+                IrcClient.Connect();
+                OnConnected.Invoke(this, new());
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (IsConnected)
+            {
+                OnDisconnected.Invoke(this, new());
+            }
+        }
+
+        public void Reconnect()
+        {
+            if (IsConnected)
+            {
+                OnReconnected.Invoke(this, new());
+            }
+        }
+    }
+}
