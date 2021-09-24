8b9c31ced9c510866d09ea9586a1943f048b832b
Sterbehilfe <twitch.tv/strbhlfe>
2021-09-24 20:11:57 +0200

    added DottedNumber test

---
+++ HLETests/NumbersTest/DottedNumberTest.cs
@@ -0,0 +1,20 @@
+﻿using HLE.Numbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HLETests.NumbersTest
+{
+    [TestClass]
+    public class DottedNumberTest
+    {
+        [TestMethod]
+        public void CreateTest()
+        {
+            long[] arr = { 123456789, 1234, 123, -123456 };
+            string[] expectation = { "123.456.789", "1.234", "123", "-123.456" };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Assert.AreEqual(new DottedNumber(arr[i]).Number, expectation[i]);
+            }
+        }
+    }
+}
