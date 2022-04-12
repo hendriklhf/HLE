0bda05869d2f30366389b72e188effdc7f626399
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-04-12 12:10:20 +0200

    some improvements

--- HLE/Emojis/EmojiFileGenerator.cs
+++ HLE/Emojis/EmojiFileGenerator.cs
@@ -25,8 +25,16 @@ namespace HLE.Emojis
 
         private readonly KeyValuePair<string, string>[] _illegalWords =
         {
-            new("100", "OneHundred"), new("+1", "ThumbUp"), new("-1", "ThumbDown"), new("T-rex", "TRex"), new("1stPlaceMedal", "FirstPlaceMedal"), new("2ndPlaceMedal", "SecondPlaceMedal"),
-            new("3rdPlaceMedal", "ThirdPlaceMedal"), new("8ball", "EightBall"), new("Non-potableWater", "NonPotableWater"), new("1234", "OneTwoThreeFour")
+            new("100", "OneHundred"),
+            new("+1", "ThumbUp"),
+            new("-1", "ThumbDown"),
+            new("T-rex", "TRex"),
+            new("1stPlaceMedal", "FirstPlaceMedal"),
+            new("2ndPlaceMedal", "SecondPlaceMedal"),
+            new("3rdPlaceMedal", "ThirdPlaceMedal"),
+            new("8ball", "EightBall"),
+            new("Non-potableWater", "NonPotableWater"),
+            new("1234", "OneTwoThreeFour")
         };
 
         public EmojiFileGenerator(string filePath, string namespaceName, char indentationChar = ' ', int indentationSize = 4)
@@ -42,15 +50,15 @@ namespace HLE.Emojis
         /// </summary>
         public void Generate()
         {
-            StringBuilder builder = new();
             HttpGet request = new("https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json");
+            StringBuilder builder = new();
             builder.Append($"#pragma warning disable 1591{Environment.NewLine}{Environment.NewLine}");
             builder.Append($"namespace {NamespaceName}{Environment.NewLine}{{{Environment.NewLine}");
             builder.Append($"{new string(IndentationChar, IndentationSize)}/// <summary>{Environment.NewLine}");
             builder.Append($"{new string(IndentationChar, IndentationSize)}/// A class that contains every existing emoji. ({DateTime.Now:dd.MM.yyyy HH:mm:ss}){Environment.NewLine}");
             builder.Append($"{new string(IndentationChar, IndentationSize)}/// </summary>{Environment.NewLine}");
             builder.Append($"{new string(IndentationChar, IndentationSize)}public static class Emoji{Environment.NewLine}{new string(IndentationChar, IndentationSize)}{{{Environment.NewLine}");
-            for (int i = 0; i <= request.Data.GetArrayLength() - 1; i++)
+            for (int i = 0; i < request.Data.GetArrayLength(); i++)
             {
                 string? name = request.Data[i].GetProperty("aliases")[0].GetString();
                 if (name is null)
@@ -58,7 +66,7 @@ namespace HLE.Emojis
                     continue;
                 }
 
-                name = $"{name[0]}".ToUpper() + name[1..];
+                name = char.ToUpper(name[0]) + name[1..];
                 string? emoji = request.Data[i].GetProperty("emoji").GetString();
                 if (emoji is null)
                 {
@@ -70,21 +78,14 @@ namespace HLE.Emojis
 
             builder.Append($"{new string(IndentationChar, IndentationSize)}}}{Environment.NewLine}}}");
             char[] charList = builder.ToString().ToCharArray();
-            for (int i = 0; i <= charList.Length - 1; i++)
+            for (int i = 0; i < charList.Length; i++)
             {
                 if (charList[i] != '_')
                 {
                     continue;
                 }
-
-                unsafe
-                {
-                    fixed (char* c = &charList[i + 1])
-                    {
-                        *c = char.ToUpper(*c);
-                        charList[i + 1] = char.ToUpper(charList[i + 1]);
-                    }
-                }
+                
+                charList[i + 1] = char.ToUpper(charList[i + 1]);
             }
 
             builder = new(charList.Where(c => c != '_').ConcatToString());
