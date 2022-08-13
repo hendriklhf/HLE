b6b56a3fde08403d3f1a6789b6fd438e92e288ad
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2022-08-14 01:47:34 +0200

    yes

--- HLE.Debug/Program.cs
+++ HLE.Debug/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Text;
-using HLE.Collections;
-using HLE.Twitch;
-using HLE.Twitch.Models;
-
-namespace HLE.Debug;
+﻿namespace HLE.Debug;
 
 #nullable disable
 
@@ -12,15 +6,5 @@ public static class Program
 {
     private static void Main()
     {
-        TwitchClient client = new()
-        {
-            ClientType = ClientType.Tcp,
-            UseSSL = true
-        };
-
-        client.JoinChannels("pietsmiet", "forsen", "xqc", "esfandtv", "summit1g", "crossmauz");
-        client.OnChatMessageReceived += (_, msg) => Console.WriteLine($"{msg.Username}: {msg.Message}");
-        client.Connect();
-        Console.ReadLine();
     }
 }
