87aeca155e1c10ad7c5ac9fb0603408a330cbf58
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2023-04-02 16:23:40 +0200

    added dictionary with double index

---
+++ HLE.Tests/CollectionsTests/DoubleDictionaryTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using HLE.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HLE.Tests.CollectionsTests;
+
+[TestClass]
+public class DoubleDictionaryTest
+{
+    [TestMethod]
+    public void AddTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+
+        Assert.AreEqual(value, dictionary[1]);
+        Assert.AreEqual(value, dictionary["a"]);
+
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            dictionary.Add(1, "a", value);
+        });
+
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            dictionary.Add(2, "a", value);
+        });
+
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            dictionary.Add(1, "b", value);
+        });
+
+        Assert.AreEqual(1, dictionary.Count);
+    }
+
+    [TestMethod]
+    public void TryAddTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+
+        Assert.IsFalse(dictionary.TryAdd(1, "a", value));
+        Assert.IsFalse(dictionary.TryAdd(1, "b", value));
+        Assert.IsFalse(dictionary.TryAdd(2, "a", value));
+
+        Assert.IsTrue(dictionary.TryAdd(2, "b", value));
+        Assert.AreEqual(2, dictionary.Count);
+    }
+
+    [TestMethod]
+    public void TryGetValueTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+
+        bool success = dictionary.TryGetValue(1, out string? retrievedValue);
+        Assert.IsTrue(success);
+        Assert.AreEqual(value, retrievedValue);
+
+        success = dictionary.TryGetValue("a", out retrievedValue);
+        Assert.IsTrue(success);
+        Assert.AreEqual(value, retrievedValue);
+
+        success = dictionary.TryGetValue(2, out retrievedValue);
+        Assert.IsFalse(success);
+        Assert.IsNull(retrievedValue);
+
+        success = dictionary.TryGetValue("b", out retrievedValue);
+        Assert.IsFalse(success);
+        Assert.IsNull(retrievedValue);
+    }
+
+    [TestMethod]
+    public void RemoveTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+        dictionary.Add(2, "b", value);
+        dictionary.Add(3, "c", value);
+
+        Assert.IsTrue(dictionary.Remove(3));
+        Assert.IsFalse(dictionary.Remove(3));
+
+        Assert.IsTrue(dictionary.Remove("b"));
+        Assert.IsFalse(dictionary.Remove("b"));
+
+        Assert.AreEqual(1, dictionary.Count);
+        Assert.AreEqual(value, dictionary[1]);
+        Assert.AreEqual(value, dictionary["a"]);
+    }
+
+    [TestMethod]
+    public void ClearTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+        dictionary.Add(2, "b", value);
+        dictionary.Add(3, "c", value);
+
+        dictionary.Clear();
+        Assert.AreEqual(0, dictionary.Count);
+    }
+
+    [TestMethod]
+    public void ContainsTest()
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+
+        Assert.IsTrue(dictionary.ContainsPrimaryKey(1));
+        Assert.IsFalse(dictionary.ContainsPrimaryKey(2));
+
+        Assert.IsTrue(dictionary.ContainsValue(value));
+        Assert.IsFalse(dictionary.ContainsValue("abc"));
+    }
+
+    [TestMethod]
+    [DataRow(5)]
+    [DataRow(10)]
+    [DataRow(50)]
+    public void CapacityTest(int capacity)
+    {
+        DoubleDictionary<int, string, string> dictionary = new();
+        const string value = "xd";
+        dictionary.Add(1, "a", value);
+
+        Assert.IsTrue(dictionary.EnsureCapacity(capacity) >= capacity);
+    }
+}
---
+++ HLE/Collections/DoubleDictionary.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace HLE.Collections;
+
+// ReSharper disable once UseNameofExpressionForPartOfTheString
+[DebuggerDisplay("Count = {Count}")]
+public sealed class DoubleDictionary<TPrimaryKey, TSecondaryKey, TValue> : IEnumerable<TValue>, IEquatable<DoubleDictionary<TPrimaryKey, TSecondaryKey, TValue>> where TPrimaryKey : notnull where TSecondaryKey : notnull
+{
+    public TValue this[TPrimaryKey key] => _values[key];
+
+    public TValue this[TSecondaryKey key] => _values[_secondaryKeyTranslations[key]];
+
+    public int Count => _values.Count;
+
+    public IEnumerable<TValue> Values => _values.Values;
+
+    private readonly Dictionary<TSecondaryKey, TPrimaryKey> _secondaryKeyTranslations = new();
+    private readonly Dictionary<TPrimaryKey, TValue> _values = new();
+
+    public DoubleDictionary()
+    {
+    }
+
+    public DoubleDictionary(int capacity)
+    {
+        _values = new(capacity);
+        _secondaryKeyTranslations = new(capacity);
+    }
+
+    public void Add(TPrimaryKey primaryKey, TSecondaryKey secondaryKey, TValue value)
+    {
+        _values.Add(primaryKey, value);
+        try
+        {
+            _secondaryKeyTranslations.Add(secondaryKey, primaryKey);
+        }
+        catch
+        {
+            _values.Remove(primaryKey);
+            throw;
+        }
+    }
+
+    public bool TryAdd(TPrimaryKey primaryKey, TSecondaryKey secondaryKey, TValue value)
+    {
+        if (!_values.TryAdd(primaryKey, value))
+        {
+            return false;
+        }
+
+        if (_secondaryKeyTranslations.TryAdd(secondaryKey, primaryKey))
+        {
+            return true;
+        }
+
+        _values.Remove(primaryKey);
+        return false;
+    }
+
+    public bool TryGetValue(TPrimaryKey key, out TValue? value)
+    {
+        return _values.TryGetValue(key, out value);
+    }
+
+    public bool TryGetValue(TSecondaryKey key, out TValue? value)
+    {
+        if (_secondaryKeyTranslations.TryGetValue(key, out TPrimaryKey? primaryKey))
+        {
+            return _values.TryGetValue(primaryKey, out value);
+        }
+
+        value = default;
+        return false;
+    }
+
+    public bool Remove(TPrimaryKey key)
+    {
+        return _values.Remove(key);
+    }
+
+    public bool Remove(TSecondaryKey key)
+    {
+        return _secondaryKeyTranslations.Remove(key, out TPrimaryKey? primaryKey) && _values.Remove(primaryKey);
+    }
+
+    public void Clear()
+    {
+        _secondaryKeyTranslations.Clear();
+        _values.Clear();
+    }
+
+    [Pure]
+    public bool ContainsPrimaryKey(TPrimaryKey key)
+    {
+        return _values.ContainsKey(key);
+    }
+
+    [Pure]
+    public bool ContainsValue(TValue value)
+    {
+        return _values.ContainsValue(value);
+    }
+
+    public int EnsureCapacity(int capacity)
+    {
+        _secondaryKeyTranslations.EnsureCapacity(capacity);
+        return _values.EnsureCapacity(capacity);
+    }
+
+    public IEnumerator<TValue> GetEnumerator()
+    {
+        return Values.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    [Pure]
+    public bool Equals(DoubleDictionary<TPrimaryKey, TSecondaryKey, TValue>? other)
+    {
+        return ReferenceEquals(this, other);
+    }
+
+    [Pure]
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as DoubleDictionary<TPrimaryKey, TSecondaryKey, TValue>);
+    }
+
+    [Pure]
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_secondaryKeyTranslations, _values);
+    }
+}
