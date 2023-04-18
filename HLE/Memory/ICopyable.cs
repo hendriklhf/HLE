afd1d6c711c16591bc3f67f64515b6de77817056
Sterbehilfe <73221731+Sterbehilfe@users.noreply.github.com>
2023-04-18 22:12:36 +0200

    added offset to CopyTo(T[])

--- HLE/Memory/ICopyable.cs
+++ HLE/Memory/ICopyable.cs
@@ -4,7 +4,7 @@ namespace HLE.Memory;
 
 public unsafe interface ICopyable<T>
 {
-    void CopyTo(T[] destination);
+    void CopyTo(T[] destination, int offset = 0);
 
     void CopyTo(Memory<T> destination);
 
