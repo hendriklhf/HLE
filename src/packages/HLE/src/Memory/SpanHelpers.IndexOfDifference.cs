d9325dcab3eebeb261212d1bbee59831f46d9d29
hendriklhf <73221731+hendriklhf@users.noreply.github.com>
2025-09-28 21:31:47 +0200

    Add `NaturalStringComparer`

---
+++ src/packages/HLE/src/Memory/SpanHelpers.IndexOfDifference.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace HLE.Memory;
+
+public static partial class SpanHelpers
+{
+    public static unsafe int IndexOfDifference<T>(ReadOnlySpan<T> first, ReadOnlySpan<T> second)
+        where T : unmanaged, IEquatable<T>
+    {
+        ref T firstRef = ref MemoryMarshal.GetReference(first);
+        ref T secondRef = ref MemoryMarshal.GetReference(second);
+
+        switch (sizeof(T))
+        {
+            case sizeof(byte):
+                return IndexOfDifference(ref Unsafe.As<T, byte>(ref firstRef), first.Length, ref Unsafe.As<T, byte>(ref secondRef), second.Length);
+            case sizeof(ushort):
+                return IndexOfDifference(ref Unsafe.As<T, ushort>(ref firstRef), first.Length, ref Unsafe.As<T, ushort>(ref secondRef), second.Length);
+            case sizeof(uint):
+                return IndexOfDifference(ref Unsafe.As<T, uint>(ref firstRef), first.Length, ref Unsafe.As<T, uint>(ref secondRef), second.Length);
+            case sizeof(ulong):
+                return IndexOfDifference(ref Unsafe.As<T, ulong>(ref firstRef), first.Length, ref Unsafe.As<T, ulong>(ref secondRef), second.Length);
+            default:
+                ThrowHelper.ThrowUnreachableException();
+                return -1;
+        }
+    }
+
+    public static int IndexOfDifference<T>(ref T first, int firstLength, ref T second, int secondLength)
+        where T : unmanaged, IEquatable<T>
+    {
+        Debug.Assert(Vector<T>.IsSupported, "Support of the generic type has to be ensured before calling this method.");
+
+        int length = Math.Min(firstLength, secondLength);
+        int start = 0;
+
+        if (Vector512.IsHardwareAccelerated && length >= Vector512<T>.Count)
+        {
+            while (length >= Vector512<T>.Count)
+            {
+                Vector512<T> firstVector = Vector512.LoadUnsafe(ref first);
+                Vector512<T> secondVector = Vector512.LoadUnsafe(ref second);
+                Vector512<T> equalsVector = Vector512.Equals(firstVector, secondVector);
+
+                ulong equalsBits = ulong.MaxValue & ~equalsVector.ExtractMostSignificantBits();
+                int index = BitOperations.TrailingZeroCount(equalsBits);
+                if (index != Vector512<T>.Count)
+                {
+                    return start + index;
+                }
+
+                first = ref Unsafe.Add(ref first, Vector512<T>.Count);
+                second = ref Unsafe.Add(ref second, Vector512<T>.Count);
+                length -= Vector512<T>.Count;
+                start += Vector512<T>.Count;
+            }
+        }
+
+        if (Vector256.IsHardwareAccelerated && length >= Vector256<T>.Count)
+        {
+            while (length >= Vector256<T>.Count)
+            {
+                Vector256<T> firstVector = Vector256.LoadUnsafe(ref first);
+                Vector256<T> secondVector = Vector256.LoadUnsafe(ref second);
+                Vector256<T> equalsVector = Vector256.Equals(firstVector, secondVector);
+
+                uint equalsBits = uint.MaxValue & ~equalsVector.ExtractMostSignificantBits();
+                int index = BitOperations.TrailingZeroCount(equalsBits);
+                if (index != Vector256<T>.Count)
+                {
+                    return start + index;
+                }
+
+                first = ref Unsafe.Add(ref first, Vector256<T>.Count);
+                second = ref Unsafe.Add(ref second, Vector256<T>.Count);
+                length -= Vector256<T>.Count;
+                start += Vector256<T>.Count;
+            }
+        }
+
+        if (Vector128.IsHardwareAccelerated && length >= Vector128<T>.Count)
+        {
+            while (length >= Vector128<T>.Count)
+            {
+                Vector128<T> firstVector = Vector128.LoadUnsafe(ref first);
+                Vector128<T> secondVector = Vector128.LoadUnsafe(ref second);
+                Vector128<T> equalsVector = Vector128.Equals(firstVector, secondVector);
+
+                uint equalsBits = uint.MaxValue & ~equalsVector.ExtractMostSignificantBits();
+                int index = BitOperations.TrailingZeroCount(equalsBits);
+                if (index != Vector128<T>.Count)
+                {
+                    return start + index;
+                }
+
+                first = ref Unsafe.Add(ref first, Vector128<T>.Count);
+                second = ref Unsafe.Add(ref second, Vector128<T>.Count);
+                length -= Vector128<T>.Count;
+                start += Vector128<T>.Count;
+            }
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            if (!first.Equals(second))
+            {
+                return start + i;
+            }
+
+            first = ref Unsafe.Add(ref first, 1);
+            second = ref Unsafe.Add(ref second, 1);
+        }
+
+        return -1;
+    }
+}
---
+++ src/packages/HLE/src/Text/NaturalStringComparer.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using HLE.Collections;
+using HLE.Marshalling;
+using HLE.Memory;
+using HLE.Numerics;
+
+namespace HLE.Text;
+
+public sealed class NaturalStringComparer : IComparer<string?>, IComparer, IEquatable<NaturalStringComparer>
+{
+    private readonly StringComparer _comparer = StringComparer.Ordinal;
+
+    private static readonly SearchValues<char> s_digitSearchValues = SearchValues.Create("0123456789");
+
+    public int Compare(object? x, object? y)
+        => Compare(x as string, y as string);
+
+    [Pure]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public int Compare(string? x, string? y)
+    {
+        if (x is null)
+        {
+            return y is null ? 0 : -1;
+        }
+
+        if (y is null)
+        {
+            return 1;
+        }
+
+        if (x.Length == 0)
+        {
+            return y.Length == 0 ? 0 : -1;
+        }
+
+        if (y.Length == 0)
+        {
+            return 1;
+        }
+
+        return CompareCore(x, y);
+    }
+
+    [Pure]
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private int CompareCore(ReadOnlySpan<char> x, ReadOnlySpan<char> y)
+    {
+        Debug.Assert(x.Length != 0);
+        Debug.Assert(y.Length != 0);
+
+        int start = SpanHelpers.IndexOfDifference(x, y);
+        if (start == -1)
+        {
+            if (x.Length == y.Length)
+            {
+                return 0;
+            }
+
+            return x.Length < y.Length ? -1 : 1;
+        }
+
+        x = x.SliceUnsafe(start);
+        y = y.SliceUnsafe(start);
+
+        if (char.IsAsciiDigit(x[0]))
+        {
+            return CompareWithNumbers(x, y);
+        }
+
+        if (char.IsAsciiDigit(y[0]))
+        {
+#pragma warning disable S2234
+            return -CompareWithNumbers(y, x);
+#pragma warning restore S2234
+        }
+
+        return NormalCompare(x, y);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private unsafe int NormalCompare(ReadOnlySpan<char> x, ReadOnlySpan<char> y)
+    {
+        nuint xSize = ObjectMarshal.GetRawStringSize(x.Length);
+        nuint size = xSize + ObjectMarshal.GetRawStringSize(y.Length) + 16;
+        byte[] buffer = Memory.ArrayPool<byte>.Shared.Rent(int.CreateChecked(size));
+
+        ref RawStringData rawX = ref StringAllocator.Alloc(buffer, x);
+        nuint alignedXSize = MemoryHelpers.Align(xSize, (uint)sizeof(nuint), AlignmentMethod.Add);
+        ref RawStringData rawY = ref StringAllocator.Alloc(buffer.AsSpan(int.CreateChecked(alignedXSize)), y);
+
+        int result = _comparer.Compare(ObjectMarshal.GetString(ref rawX), ObjectMarshal.GetString(ref rawY));
+        Memory.ArrayPool<byte>.Shared.Return(buffer);
+        return result;
+    }
+
+    private static int CompareWithNumbers(ReadOnlySpan<char> x, ReadOnlySpan<char> y)
+    {
+        // x starts with a digit
+        // y might start with a digit or not
+
+        Debug.Assert(char.IsAsciiDigit(x[0]));
+
+        if (!char.IsAsciiDigit(y[0]))
+        {
+            return -1;
+        }
+
+        int numberEndX = x.IndexOfAnyExcept(s_digitSearchValues);
+        int numberEndY = y.IndexOfAnyExcept(s_digitSearchValues);
+
+        ulong numberX = ulong.Parse(x.SliceUnsafe(..Unsafe.BitCast<int, Index>(numberEndX)));
+        ulong numberY = ulong.Parse(y.SliceUnsafe(..Unsafe.BitCast<int, Index>(numberEndY)));
+
+        if (numberX != numberY)
+        {
+            return numberX < numberY ? -1 : 1;
+        }
+
+        return 0;
+    }
+
+    [Pure]
+    public bool Equals([NotNullWhen(true)] NaturalStringComparer? other) => ReferenceEquals(this, other);
+
+    [Pure]
+    public override bool Equals([NotNullWhen(true)] object? obj) => ReferenceEquals(this, obj);
+
+    [Pure]
+    public override int GetHashCode() => RuntimeHelpers.GetHashCode(this);
+
+    public static bool operator ==(NaturalStringComparer? left, NaturalStringComparer? right) => Equals(left, right);
+
+    public static bool operator !=(NaturalStringComparer? left, NaturalStringComparer? right) => !(left == right);
+}
